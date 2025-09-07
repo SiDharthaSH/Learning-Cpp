15af41bdfd516861e38fd9d4ca01054db0a307da
SiDharthaSH <sidharthashankarhazra@gmail.com>
2025-09-07 14:52:35 +0530

    separate the conditional preprocessor declaration into a header file

--- src/1_hello_world.cpp
+++ src/1_hello_world.cpp
@@ -1,24 +1,6 @@
 #include <iostream>
 
-void keep_window_open();
-
-#if defined(_WIN32) // defined when compiled in win32/64 env
-  void keep_window_open() {
-    std::cout.clear();
-    std::cout << "Enter any charater to exit" << std::endl;
-    char ch;
-    std::cin >> ch;
-    return;
-  }
-#elif defined(__linux__) // defined when compiled in linux env
-  void keep_window_open() {
-    return;
-  }
-#else // any other OS
-  void keep_window_open() {
-    return;
-  }
-#endif
+#include "std_lib_facilities.hpp"
 
 int main() {
   std::cout << "Hello, World!" << std::endl;
