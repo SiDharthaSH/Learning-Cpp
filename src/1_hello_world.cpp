1847b952bd1c250b4ebf498717581d39e236c1c8
SiDharthaSH <sidharthashankarhazra@gmail.com>
2025-09-07 14:29:33 +0530

    hold terminal in windows before exiting

--- src/1_hello_world.cpp
+++ src/1_hello_world.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 
-using namespace std;
+void keep_window_open();
+
+#if defined(_WIN32) // defined when compiled in win32/64 env
+  void keep_window_open() {
+    std::cout.clear();
+    std::cout << "Enter any charater to exit" << std::endl;
+    char ch;
+    std::cin >> ch;
+    return;
+  }
+#elif defined(__linux__) // defined when compiled in linux env
+  void keep_window_open() {
+    return;
+  }
+#else // any other OS
+  void keep_window_open() {
+    return;
+  }
+#endif
 
 int main() {
-  cout << "Hello, World!" << endl;
+  std::cout << "Hello, World!" << std::endl;
+  keep_window_open();
+  return 0; // Ignored by Windows
 }
+
